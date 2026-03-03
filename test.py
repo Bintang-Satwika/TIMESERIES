4cbc587679b5b4b557c6f7f0412149eef0ca59b7
Satwika <lab_profben@example.com>
2026-03-03 11:41:56 +0700

    03_03_2026

---
+++ test.py
@@ -0,0 +1,5 @@
+import torch
+
+# print(f"GPU: {torch.cuda.get_device_name()}")
+# print(f"Capability: {torch.cuda.get_device_capability()}")
+# print(f"VRAM: {torch.cuda.get_device_properties(0).total_memory/1024**3:.1f}GB")
